b2e3a7e788b3326bfd65e382d87f58266752bce0
J. B. Rainsberger <me@jbrains.ca>
2013-10-14 15:38:02 -0300

    The test now clearly describes what it thinks is a mistake.

--- spec/learn_code_block_spec.rb
+++ spec/learn_code_block_spec.rb
@@ -7,7 +7,9 @@ require "plugins/code_block"
 describe "Parsing parameters for codeblock" do
   example "only a URL" do
     results = Jekyll::CodeBlock.parse_tag_parameters("https://gist.github.com/1234")
-    results[:filetype].should be_nil
+    pending("Filetype appears to be interpreted incorrectly") do
+      results[:filetype].should be_nil
+    end
   end
 end
 
