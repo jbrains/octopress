0bdd5c52dc045fe4006b13825ee969f4ef77dc82
J. B. Rainsberger <me@jbrains.ca>
2013-10-14 15:00:41 -0300

    FAILING: Try to instantiate a CodeBlock

--- spec/learn_code_block_spec.rb
+++ spec/learn_code_block_spec.rb
@@ -1,9 +1,17 @@
+# SMELL Production code needs Jekyll, but doesn't require it, so this file has to.
+require "jekyll"
+
 require "rspec"
 require "plugins/code_block"
 
 describe "Parsing parameters for codeblock" do
-  example "Am I requiring everything correctly?" do
-    Jekyll::CodeBlock.new
+  def code_block_with_url(url)
+    irrelevant_tokens = []
+    Jekyll::CodeBlock.new("irrelevant tag name", "https://gist.github.com/1234", irrelevant_tokens)
+  end
+
+  example "only a URL" do
+    code_block_with_url("https://gist.github.com/1234")
   end
 end
 
