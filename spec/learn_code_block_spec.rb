8a7d5b610d0592899e5f90436892915a5e23cee5
J. B. Rainsberger <me@jbrains.ca>
2013-10-14 13:58:22 -0300

    Fixed spec name so that I could include it as code in a codeblock in a blog post. (Yup.)

--- spec/learn_code_block_spec.rb
+++ spec/learn_code_block_spec.rb
@@ -1,7 +1,7 @@
 require "rspec"
 require "plugins/code_block"
 
-describe "Parsing parameters for {% codeblock %}" do
+describe "Parsing parameters for codeblock" do
   example "Am I requiring everything correctly?" do
     Jekyll::CodeBlock.new
   end
