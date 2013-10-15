7ca5117fa7604c977b3cdb4b426a5bd1b95ac0eb
J. B. Rainsberger <me@jbrains.ca>
2013-10-15 18:04:22 -0300

    Found another potential mistake in CodeBlock

--- spec/learn_code_block_spec.rb
+++ spec/learn_code_block_spec.rb
@@ -11,5 +11,13 @@ describe "Parsing parameters for codeblock" do
       results[:filetype].should be_nil
     end
   end
+
+  example "all advertised parameters" do
+    results = Jekyll::CodeBlock.parse_tag_parameters("A nice, simple caption gist2.rb see more")
+    # SMELL caption is captionHtml, rather than caption text; more context dependence
+    pending("Another mistake: the 'file' and 'caption' properties are wrong, and it appears that nobody's using the 'file' property.") do
+      results.should == {filetype: "rb", file: "gist2.rb", caption: "<figcaption><span>A nice, simple caption</span></figcaption>\n"}
+    end
+  end
 end
 
