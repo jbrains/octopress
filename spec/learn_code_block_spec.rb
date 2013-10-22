e194ad22e5cd76c982022ddeffe62a60de9d5549
J. B. Rainsberger <me@jbrains.ca>
2013-10-22 18:34:03 -0300

    Inlined obsolete temporary variables.

--- spec/learn_code_block_spec.rb
+++ spec/learn_code_block_spec.rb
@@ -7,25 +7,21 @@ require "plugins/code_block"
 describe "Parsing parameters for codeblock" do
   # The @file field appears to be unused, so don't bother checking it
   example "only a URL" do
-    results = Jekyll::CodeBlock.parse_tag_parameters("https://gist.github.com/1234 ")
-    results.should include({
+    Jekyll::CodeBlock.parse_tag_parameters("https://gist.github.com/1234 ").should include({
       caption: %Q{<figcaption><span>https://gist.github.com/1234</span><a href='https://gist.github.com/1234'>#{'link'}</a></figcaption>},
       filetype: nil
     })
   end
 
   example "only a title that does not look like a filename" do
-    # SMELL This really should not expect a newline at the end of the caption; that's just stupid
-    results = Jekyll::CodeBlock.parse_tag_parameters("anything that does not contain a dot")
-    results.should include({ 
+    Jekyll::CodeBlock.parse_tag_parameters("anything that does not contain a dot").should include({ 
       filetype: nil,
       caption: %Q{<figcaption><span>anything that does not contain a dot</span></figcaption>}
     })
   end
 
   example "a single-word title that looks like a filename" do
-    results = Jekyll::CodeBlock.parse_tag_parameters("filename.xyz")
-    results.should include({ 
+    Jekyll::CodeBlock.parse_tag_parameters("filename.xyz").should include({ 
       filetype: "xyz",
       caption: %Q{<figcaption><span>filename.xyz</span></figcaption>}
     })
@@ -35,8 +31,7 @@ describe "Parsing parameters for codeblock" do
   example "a multiple-word title that looks like it has a filename in the middle"
 
   example "all advertised parameters" do
-    results = Jekyll::CodeBlock.parse_tag_parameters("A nice, simple caption for gist2.rb http://www.jbrains.ca see more")
-    results.should include({
+    Jekyll::CodeBlock.parse_tag_parameters("A nice, simple caption for gist2.rb http://www.jbrains.ca see more").should include({
       filetype: "rb", 
       caption: %Q{<figcaption><span>A nice, simple caption for gist2.rb</span><a href='http://www.jbrains.ca'>see more</a></figcaption>}
     })
